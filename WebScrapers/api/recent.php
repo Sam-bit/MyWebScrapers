a9e278c457c812987a91109eb081ca1f8303d297
Sambit Samal <itsme.sambit1991@gmail.com>
2020-05-31 18:39:07 +0530

    Add files via upload

--- WebScrapers/api/recent.php
+++ WebScrapers/api/recent.php
@@ -2,7 +2,7 @@
 require_once('db.php');
 $offset = $_POST["offset"];
 $recordsPerPage = $_POST["records_per_page"];
-$query = "select a.article_url,a.article_title,a.article_thumbnail,a.article_date,a.article_subtitle,a.article_content,a.article_checked_by,a.article_verdict,
+$query = "select a.article_url,a.article_title,a.article_thumbnail,a.article_date,a.article_subtitle,a.article_checked_by,a.article_verdict,
 a.article_alt_verdict,a.article_site_id, s.src_name From articles a inner join sources s on a.article_site_id = s.src_id order by a.article_date desc limit :offset, :recordsPerPage";
 $stmt = $db->prepare($query);
 $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
